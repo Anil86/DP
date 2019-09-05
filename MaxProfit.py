79eff408da37d6a15d8c137c88380c57fba2a4af
Anil Kumar <anil.apex@outlook.com>
2019-09-05 14:24:31 +0530

    Solved: Maximum Profit using DP.
    Related Work Items: #774

--- MaxProfit.py
+++ MaxProfit.py
@@ -2,43 +2,56 @@ from enum import Enum
 
 
 class MaxProfit:
-    _count = 0
-
     def CollectMax(self, array1, array2, penalty):
+        dp = [[None] * 2 for _ in range(len(array1) + 1)]
+
         def CollectMax(i, previousArray):
-            MaxProfit._count += 1
             # Solve small sub-problems
-            if i == len(array1): return 0
+            if i == len(array1):
+                dp[i][previousArray.value] = 0
+                return 0
 
             # Divide
-            p = 0 if i == 0 else penalty
+            p = 0 if i == 0 else penalty   # No penalty when picking 1st item
 
-            # Pick array1
-            maxA = array1[i] + CollectMax(i + 1, PreviousArray.Array1) if previousArray == PreviousArray.Array1 else \
-                array1[i] - p + CollectMax(i + 1, PreviousArray.Array1)
+            # Choice 1: Pick array1
+            if dp[i + 1][PreviousArray.Array1.value] is None:
+                dp[i + 1][PreviousArray.Array1.value] = CollectMax(i + 1, PreviousArray.Array1)
+            # If previous from array1, pick current array1 & find max from remaining
+            # If previous from array2, pick current array1, deduct penalty & find max from remaining
+            maxA = array1[i] + dp[i + 1][PreviousArray.Array1.value] \
+                if previousArray == PreviousArray.Array1 else \
+                array1[i] - p + dp[i + 1][PreviousArray.Array1.value]
 
-            # Pick array2
-            maxB = array2[i] - p + CollectMax(i + 1, PreviousArray.Array2) if previousArray == PreviousArray.Array1 else \
-                array2[i] + CollectMax(i + 1, PreviousArray.Array2)
+            # Choice 2: Pick array2
+            if dp[i + 1][PreviousArray.Array2.value] is None:
+                dp[i + 1][PreviousArray.Array2.value] = CollectMax(i + 1, PreviousArray.Array2)
+            # If previous from array1, pick current array2, deduct penalty & find max from remaining
+            # If previous from array2, pick current array2 & find max from remaining
+            maxB = array2[i] - p + dp[i + 1][PreviousArray.Array2.value] \
+                if previousArray == PreviousArray.Array1 else \
+                array2[i] + dp[i + 1][PreviousArray.Array2.value]
 
             # Combine
-            return max(maxA, maxB)
+            # Find max from choice 1 & 2
+            dp[i][previousArray.value] = max(maxA, maxB)
+            return dp[i][previousArray.value]
 
+        # Start from 1st item & previous array = array1
         return CollectMax(0, PreviousArray.Array1)
 
     @staticmethod
     def Work():
-        # array1 = [1, 2, 3, 4, 7, 1]
-        # array2 = [4, 5, 1, 4, 1, 7]
-        # penalty = 2   # Ans: 26   # Calls: 127
-
         array1 = [1, 2, 3]
         array2 = [4, 5, 1]
-        penalty = 2  # Ans: 10   # Calls: 15
+        penalty = 2  # Ans: 10
+
+        # array1 = [1, 2, 3, 4, 7, 1]
+        # array2 = [4, 5, 1, 4, 1, 7]
+        # penalty = 2  # Ans: 26
 
         maxTotal = MaxProfit().CollectMax(array1, array2, penalty)
         print(maxTotal)
-        print(MaxProfit._count)
 
 
-PreviousArray = Enum("PreviousArray", ["Array1", "Array2"])
+PreviousArray = Enum("PreviousArray", {"Array1": 0, "Array2": 1})
