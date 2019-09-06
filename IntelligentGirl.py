ac697c8cacb145ca66a03eb5bced9b8b67239062
Anil Kumar <anil.apex@outlook.com>
2019-09-06 17:25:23 +0530

    Solved: Intelligent girl using DP
    Related Work Items: #774

--- IntelligentGirl.py
+++ IntelligentGirl.py
@@ -1,23 +1,34 @@
 class IntelligentGirl:
     def CountEvens(self, numbers):
+        dp = [-1] * (len(numbers) + 1)
+
         def CountEvens(i):
             # Solve small sub-problems
-            if i == len(numbers): return 0
+            if i == len(numbers):
+                dp[i] = 0
+                return 0
 
             # Divide
             currentEvenCount = 1 if numbers[i] % 2 == 0 else 0
 
-            remainingEvensCount = CountEvens(i + 1)
+            if dp[i + 1] == -1: CountEvens(i + 1)
+            remainingEvensCount = dp[i + 1]
 
             # Combine
-            return currentEvenCount + remainingEvensCount
+            dp[i] = currentEvenCount + remainingEvensCount
+            return dp[i]
 
-        return CountEvens(0)
+        countEvens = ""
+        for i in range(len(numbers)):
+            if dp[i] == -1: CountEvens(i)
+            countEven = dp[i]
+            countEvens += f" {countEven}"
+
+        print(countEvens.lstrip())
 
     @staticmethod
     def Work():
-        # numbers = [3, 2, 7, 8]
-        numbers = [5, 7, 4, 6, 7, 4, 5, 4, 6, 4, 7, 6]
+        numbers = [3, 2, 7, 8]   # Ans: 2 2 1 1
+        # numbers = [5, 7, 4, 6, 7, 4, 5, 4, 6, 4, 7, 6]  # Ans: 7 7 7 6 5 5 4 4 3 2 1 1
 
-        countEvens = IntelligentGirl().CountEvens(numbers)
-        print(countEvens)
+        IntelligentGirl().CountEvens(numbers)
