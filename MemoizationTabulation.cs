e955f120aa4edc72e671d1b85b4169a3a5ee4d86
Anil Kumar <anil.apex@outlook.com>
2019-09-13 04:20:17 +0530

    Tabulation demo
    Related Work Items: #774

--- MemoizationTabulation.cs
+++ MemoizationTabulation.cs
@@ -13,7 +13,6 @@ namespace DP
 
             long FibonacciMemoizationLocal(int n)
             {
-                _count++;
                 // Solve small sub-problems
                 if (n <= 1) return dp[n] = n;
 
@@ -31,16 +30,40 @@ namespace DP
         }
 
 
-        private static int _count;
+
+        private long FibonacciTabulation(int num)
+        {
+            long[] dp = new long[num + 1];
+
+            // Solve small sub-problems
+            dp[0] = 0;
+            dp[1] = 1;
+
+            for (int i = 2; i <= num; i++)
+            {
+                // Divide
+                long augend = dp[i - 1];   // Augend
+                long addend = dp[i - 2];   // Addend
+
+
+                // Combine
+                dp[i] = augend + addend;
+            }
+
+            return dp[num];
+        }
+
+
+
         internal static void Work()
         {
-            //int n = 5;   // Ans: 5   Count: 15
+            int n = 5;   // Ans: 5   Count: 15
             //int n = 20;   // Ans: 6765   Count: 21891   21
-            int n = 50;   // Ans: 12586269025   Count: Very long time   51
+            //int n = 50;   // Ans: 12586269025   Count: Very long time   51
 
-            long fibonacciNumber = new MemoizationTabulation().FibonacciMemoization(n);
+            //long fibonacciNumber = new MemoizationTabulation().FibonacciMemoization(n);
+            long fibonacciNumber = new MemoizationTabulation().FibonacciTabulation(n);
             WriteLine(fibonacciNumber);
-            WriteLine($"Count: {_count}");
         }
     }
 }
